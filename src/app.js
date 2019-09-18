460279e4fefa02c2b42fc993cd75667ae8906abe
Cory O'Brien <cory@prtcl.cc>
2019-09-18 01:57:55 -0400

    fix viewport height in safari

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
 import './app.less';
 
+const resize = () => {
+  const height = `${window.innerHeight}px`;
+
+  document.body.height = document.body.style.height = height;
+};
+
 const run = () => {
   const video = document.querySelector('.rain');
 
-  video.play();
-  video.playbackRate = 0.9;
+  video.playbackRate = 0.8;
 
   document.addEventListener('touchend', () => {
     video.play();
   });
+
+  resize();
 };
 
+window.addEventListener('resize', resize);
 window.addEventListener('load', run);
