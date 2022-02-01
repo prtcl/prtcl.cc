6af13c3d59520cbabdc392a598d254013025be3a
Cory O'Brien <cory@prtcl.cc>
2022-02-01 00:38:16 -0500

    basic text and background image

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
+import { h, text, app } from 'hyperapp';
 import './app.less';
+
+const App = () =>
+  h('main', { class: 'main' }, [
+    h('p', {}, text('Cory O\'Brien is a ')),
+    h('p', {}, h('a', { href: 'https://github.com/prtcl' }, text('software engineer'))),
+    h('p', {}, text(' and ')),
+    h('p', {}, h('a', { href: 'https://coryobrien.bandcamp.com' }, text('sound artist'))),
+    h('p', {}, text(' who lives in NYC')),
+  ]);
+
+app({
+  node: document.getElementById('app'),
+  view: App,
+});
