4b4af397bbd157d54d08d9a2a3655a8ba7f9d1e4
Cory O'Brien <cory@prtcl.cc>
2022-02-02 23:27:15 -0500

    make style cooler

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ import { h, text, app } from 'hyperapp';
 import './app.less';
 
 const App = () =>
-  h('main', { class: 'main' }, [
-    h('p', {}, text('Cory O\'Brien is a ')),
-    h('p', {}, h('a', { href: 'https://github.com/prtcl' }, text('software engineer'))),
-    h('p', {}, text(' and ')),
-    h('p', {}, h('a', { href: 'https://coryobrien.bandcamp.com' }, text('sound artist'))),
-    h('p', {}, text(' who lives in NYC')),
+  h('main', { class: 'container' }, [
+    h('article', {}, [
+      h('p', {}, text('Cory O\'Brien is a ')),
+      h('p', {}, h('a', { href: 'https://github.com/prtcl' }, text('software engineer'))),
+      h('p', {}, text(' and ')),
+      h('p', {}, h('a', { href: 'https://coryobrien.bandcamp.com' }, text('sound artist'))),
+      h('p', {}, text(' who lives in NYC')),
+    ]),
   ]);
 
 app({
