802a0f3d8896df80ad1fed21920e97ef98cd5c1a
Cory O'Brien <cory@prtcl.cc>
2022-09-24 11:58:59 -0400

    move template meta tags to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,47 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const AUTHOR = 'Cory O\'Brien';
+const DESCRIPTION = 'NYC-based artist and performer';
+const CANONICAL = 'http://prtcl.cc';
+
+const IMAGE = {
+  URL: 'http://prtcl.cc/assets/tree.jpg',
+  TYPE: 'image/jpeg',
+  WIDTH: '1420',
+  HEIGHT: '1904',
+};
 
 module.exports = {
   entry: './src/app.js',
   output: {
     path: path.resolve('dist', 'assets'),
     publicPath: '/assets/',
-    filename: 'app.js'
+    filename: '[name].[contenthash].js',
   },
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      title: AUTHOR,
+      template: path.resolve('src', 'html', 'index.html'),
+      meta: {
+        author: AUTHOR,
+        description: DESCRIPTION,
+        viewport: 'width=device-width, initial-scale=1, user-scalable=no',
+        'google-site-verification': 'Ydq_4fjxlJ7tZVQZzS4DXQ1VDqWU7lCIT_fiw_HUJlQ',
+        'og:title': { property: 'og:title', content: AUTHOR, },
+        'og:url': { property: 'og:url', content: CANONICAL },
+        'og:description': { property: 'og:description', content: DESCRIPTION },
+        'og:image': { property: 'og:image', content: IMAGE.URL, },
+        'og:image:type': { property: 'og:image:type', content: IMAGE.TYPE, },
+        'og:image:width': { property: 'og:image:width', content: IMAGE.WIDTH, },
+        'og:image:height': { property: 'og:image:height', content: IMAGE.HEIGHT },
+      }
+    }),
+  ],
   module: {
     rules: [
       {
