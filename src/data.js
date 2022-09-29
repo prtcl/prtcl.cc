93183a9e367e3e15e96b9b993f9269c4bf6bb763
Cory O'Brien <cory@prtcl.cc>
2022-09-29 17:40:50 -0400

    move data to its own file

---
+++ src/data.js
@@ -0,0 +1,65 @@
+export const embeds = {
+  asliomar: {
+    width: '100%',
+    height: '300',
+    scrolling: 'no',
+    frameborder: 'no',
+    allow: 'autoplay',
+    src: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1069275841&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+  },
+  twoBoats: {
+    style: { border: 0, width: '100%', height: '120px' },
+    src: 'https://bandcamp.com/EmbeddedPlayer/album=2683632007/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/',
+    seamless: true,
+  },
+};
+
+export const links = [
+  {
+    title: 'Bandcamp',
+    url: 'https://coryobrien.bandcamp.com',
+  },
+  {
+    title: 'Github',
+    url: 'https://github.com/prtcl'
+  },
+  {
+    title: 'cory@prtcl.cc',
+    url: 'mailto:cory@prtcl.cc'
+  }
+];
+
+export const projects = [
+  {
+    title: 'Release: Two Boats with Acoustic and Digital Resonators',
+    url: 'https://coryobrien.bandcamp.com/album/two-boats-with-acoustic-and-digital-resonators'
+  },
+  {
+    title: 'Release: Central Park Binaural',
+    url: 'https://coryobrien.bandcamp.com/album/central-park-binaural',
+  },
+  {
+    title: 'Release: Raw Croton',
+    url: 'https://coryobrien.bandcamp.com/album/raw-croton'
+  },
+  {
+    title: 'Release: Midtown Binaural',
+    url: 'https://coryobrien.bandcamp.com/album/midtown-binaural',
+  },
+  {
+    title: 'Compilation: Isolation',
+    url: 'https://fuzzypanda.bandcamp.com/album/isolation',
+  },
+  {
+    title: 'Software: TiberSynth',
+    url: 'https://tibersynth.cc/'
+  },
+  {
+    title: 'Release: Sympathetic Field Matrix',
+    url: 'https://coryobrien.bandcamp.com/album/sympathetic-field-matrix',
+  },
+  {
+    title: 'Release: Denser Materials',
+    url: 'https://coryobrien.bandcamp.com/album/denser-materials',
+  },
+];
