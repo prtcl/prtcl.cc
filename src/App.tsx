147724f70605b2f7ecb668911f9cf032f2b8fd73
Cory O'Brien <cory@prtcl.cc>
2023-01-21 15:47:13 -0500

    simple bio component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.less';
+import Bio from './components/Bio';
+import styles from './App.less';
 
 const App = () => {
   return (
-    <div>Hello</div>
-  )
-}
+    <div className={styles.container}><Bio /></div>
+  );
+};
 
 export default App;
---
+++ src/components/Bio/Bio.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Bio.less';
+
+const Bio = () => {
+  return (
+    <div className={styles.container}>
+      <p>Cory O'Brien is a software engineer and sound artist who lives in NYC</p>
+    </div>
+  );
+};
+
+export default Bio;
