21217781459f27658aad985c172ac263693bd40b
Cory O'Brien <cory@prtcl.cc>
2023-01-21 21:59:27 -0500

    render first polygon in an animation loop

--- src/components/Visualization/Visualization.tsx
+++ src/components/Visualization/Visualization.tsx
@@ -1,10 +1,47 @@
 import React, { useCallback } from 'react';
-import Canvas from '../Canvas';
+import { scale } from 'plonk';
+import Canvas, { CanvasAPI, Dimensions, PolygonCoord } from '../Canvas';
+import useVisualizationState, { Polygon } from './hooks/useVisualizationState';
+
+const scalePolygonToCoordinates = (polygon: Polygon, dimensions: Dimensions) => {
+  const { points } = polygon;
+  const coords: PolygonCoord[] = [];
+  const end = points.length;
+  let p = 0;
+
+  while (p < end) {
+    const point = points[p];
+
+    coords.push([
+      scale(point.x, 0, 1, 0, dimensions.width),
+      scale(point.y, 0, 1, 0, dimensions.height),
+    ]);
+
+    p++;
+  }
+
+  return coords;
+};
 
 const Visualization = () => {
-  const handleTick = useCallback(() => {
-    console.log(Date.now());
-  }, []);
+  const state = useVisualizationState();
+
+  console.log(state.current);
+
+  const handleTick = useCallback((dimensions: Dimensions, helpers: CanvasAPI) => {
+    const { width, height } = dimensions;
+    const { alpha, clear, drawPolygon, stroke, strokeWeight } = helpers;
+
+    const { polygons } = state.current;
+    const first = polygons[0];
+    const coords = scalePolygonToCoordinates(first, dimensions);
+
+    // clear(width, height);
+    stroke(0, 0, 0);
+    strokeWeight(1);
+    alpha(first.strength);
+    drawPolygon(coords, false);
+  }, [state]);
 
   return <Canvas draw={handleTick} />;
 };
--- src/components/Visualization/hooks/useVisualizationState.ts
+++ src/components/Visualization/hooks/useVisualizationState.ts
@@ -1,7 +1,7 @@
-import { useRef } from 'react';
-import { drunk, exp, rand, scale } from 'plonk';
+import { useRef, useEffect, useCallback } from 'react';
+import { drunk, exp, ms, rand, scale } from 'plonk';
 
-type Point = {
+export type Point = {
   id: number;
   x: number;
   y: number;
@@ -11,7 +11,7 @@ type Point = {
   };
 };
 
-type Polygon = {
+export type Polygon = {
   id: number;
   points: Point[];
   strength: number;
@@ -26,6 +26,8 @@ const N_POLYGONS = 100;
 const N_POINTS = 4;
 const BOUNDS_MIN = 0.05;
 const BOUNDS_MAX = 0.95;
+const SPEED_MIN = 0.001;
+const SPEED_MAX = 0.01;
 
 const initializePolygons = () => {
   const polygons: Polygon[] = [];
@@ -66,12 +68,63 @@ const initializeState = (): VisualizationState => {
 
   return {
     polygons,
-    speed: 0.002,
+    speed: (SPEED_MAX - SPEED_MIN) / 2,
   };
+};
+
+const speedDrunk = drunk(SPEED_MIN, SPEED_MAX);
+
+const updateFirstPolygon = (state: VisualizationState) => {
+  const { polygons } = state;
+  const firstPolygon = polygons[0];
+  const end = firstPolygon.points.length;
+  let p = 0;
+
+  while (p < end) {
+    const point = firstPolygon.points[p];
+    let hasHitBounds = false;
+
+    if (point.x >= BOUNDS_MAX) {
+      point.direction.x = rand(-1, 0);
+      hasHitBounds = true;
+    } else if (point.x <= BOUNDS_MIN) {
+      point.direction.x = rand(0, 1);
+      hasHitBounds = true;
+    }
+
+    if (point.y >= BOUNDS_MAX) {
+      point.direction.y = rand(-1, 0);
+      hasHitBounds = true;
+    } else if (point.y <= BOUNDS_MIN) {
+      point.direction.y = rand(0, 1);
+      hasHitBounds = true;
+    }
+
+    if (hasHitBounds) {
+      state.speed = speedDrunk();
+    }
+
+    point.x += (point.direction.x * state.speed);
+    point.y += (point.direction.y * state.speed);
+
+    p++;
+  }
 };
 
 const useVisualizationState = () => {
   const state = useRef<VisualizationState>(initializeState());
+
+  const tickHandler = useCallback(() => {
+    updateFirstPolygon(state.current);
+  }, [state]);
+
+  useEffect(() => {
+    const timerId = setInterval(tickHandler, ms('60fps'));
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
 
   return state;
 };
