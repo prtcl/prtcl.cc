66b6321653e54b3f0ba4d74fd65cbde11392fa88
Cory O'Brien <cory@prtcl.cc>
2023-09-03 14:57:24 -0400

    first draft of rand and drunk

--- src/lib/plonk/index.ts
+++ src/lib/plonk/index.ts
@@ -1,3 +1,4 @@
+export { default as clamp } from './maths/clamp';
 export { default as Frames } from './timers/Frames';
 export {
   default as Metro,
@@ -6,3 +7,14 @@ export {
   type TimerState,
 } from './timers/Metro';
 export { default as useMetro } from './hooks/useMetro';
+export {
+  default as Rand,
+  type RandOptions,
+  type RandState,
+} from './maths/Rand';
+export {
+  default as Drunk,
+  DEFAULT_DRUNK_STEP,
+  type DrunkOptions,
+  type DrunkState,
+} from './maths/Drunk';
