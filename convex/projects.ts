d51f4b5fb5951ef61d4ac54f7b870ccc71ebe76d
Cory O'Brien <cory@prtcl.cc>
2024-05-08 18:15:12 -0400

    add migration for reordering projects

--- convex/projects.ts
+++ convex/projects.ts
@@ -1,4 +1,6 @@
-import { query } from './_generated/server';
+import { v } from 'convex/values';
+import { type Id } from './_generated/dataModel';
+import { internalMutation, query } from './_generated/server';
 
 export const loadProjects = query({
   args: {},
@@ -9,5 +11,43 @@ export const loadProjects = query({
       .collect();
 
     return projects.sort((a, b) => a.order - b.order);
+  },
+});
+
+export const reorderProjects = internalMutation({
+  args: {
+    id: v.id('projects'),
+    order: v.number(),
+  },
+  handler: async (ctx, { id: targetId, order: targetOrder }) => {
+    const projects = await ctx.db.query('projects').collect();
+    const { updates } = projects
+      .sort((a, b) => a.order - b.order)
+      .reduce<{ updates: Map<Id<'projects'>, number>; prev: number }>(
+        (res, project) => {
+          let updatedOrder;
+
+          if (project._id === targetId) {
+            updatedOrder = targetOrder;
+          } else if (project.deletedAt) {
+            updatedOrder = Number.MAX_SAFE_INTEGER;
+          } else {
+            const next = res.prev + 1;
+            updatedOrder = next === targetOrder ? next + 1 : next;
+            res.prev = updatedOrder;
+          }
+
+          res.updates.set(project._id, updatedOrder);
+
+          return res;
+        },
+        { updates: new Map(), prev: -1 },
+      );
+
+    for (const [id, order] of updates.entries()) {
+      await ctx.db.patch(id, { order });
+    }
+
+    return updates.entries();
   },
 });
--- convex/schema.ts
+++ convex/schema.ts
@@ -1,11 +1,19 @@
 import { defineSchema, defineTable } from 'convex/server';
 import { v } from 'convex/values';
 
+const categories = v.union(
+  v.literal('code'),
+  v.literal('sound'),
+  v.literal('text'),
+  v.literal('video'),
+);
+
 const projects = defineTable({
-  category: v.union(v.literal('code'), v.literal('sound'), v.literal('press')),
+  category: categories,
   deletedAt: v.union(v.number(), v.null()),
-  label: v.string(),
   order: v.number(),
+  publishedAt: v.union(v.number(), v.null()),
+  title: v.string(),
   url: v.string(),
 });
 
