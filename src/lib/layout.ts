f3eaefe74446a555239beaaefe2f5b00ebb26a05
Cory O'Brien <cory@prtcl.cc>
2024-05-08 14:23:10 -0400

    fix layout viz scrolling on mobile

--- src/lib/layout.ts
+++ src/lib/layout.ts
@@ -2,8 +2,11 @@ import { Box, styled } from 'styled-system/jsx';
 
 export const Container = styled(Box, {
   base: {
-    width: '100%',
     height: '100%',
+    inset: 0,
+    pointerEvents: 'none',
+    position: 'fixed',
+    width: '100%',
   },
 });
 
@@ -14,7 +17,7 @@ export const Overlay = styled(Box, {
     left: 0,
     overflowX: 'hidden',
     overflowY: 'auto',
-    position: 'fixed',
+    position: 'absolute',
     top: 0,
     width: '100%',
     zIndex: 9999,
