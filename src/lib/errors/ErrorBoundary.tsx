2653f3a89b65dee1ada6a9185033330477ca4c66
Cory O'Brien <cory@prtcl.cc>
2024-11-07 14:21:19 -0500

    add error boundary to handle convex throws

---
+++ src/lib/errors/ErrorBoundary.tsx
@@ -0,0 +1,39 @@
+import { PureComponent, type PropsWithChildren, type ReactNode } from 'react';
+
+export type ErrorState = {
+  hasError: boolean;
+  error: Error | any | null;
+};
+
+export type ErrorBoundaryProps = PropsWithChildren<{
+  fallback?: (state: ErrorState) => ReactNode;
+}>;
+
+export class ErrorBoundary extends PureComponent<
+  ErrorBoundaryProps,
+  ErrorState
+> {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: null };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: !!error, error };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error(error, errorInfo);
+  }
+
+  render() {
+    const { children, fallback } = this.props;
+    const { hasError } = this.state;
+
+    if (hasError && fallback) {
+      return fallback(this.state);
+    }
+
+    return children;
+  }
+}
