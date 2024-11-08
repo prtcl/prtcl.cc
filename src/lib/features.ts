e50240243d2fbc91f14f59d0cdeb9d7353512dd2
Cory O'Brien <cory@prtcl.cc>
2024-11-08 12:14:18 -0500

    add feature override and some utility types

--- src/lib/features.ts
+++ src/lib/features.ts
@@ -1,23 +1,43 @@
 import { useQuery } from 'convex/react';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { api } from '~/convex/api';
+import { createEnumGuard, type EnumToString } from '~/types/utils';
 
 export enum FeatureFlags {
   PROJECT_PREVIEWS = 'isProjectPreviewsEnabled',
   PROJECT_VIEWER = 'isProjectViewerEnabled',
 }
 
-export type FeaturesMap = Map<`${FeatureFlags}`, boolean>;
+type FlagValues = EnumToString<typeof FeatureFlags>;
+export type FeaturesMap = Map<FlagValues, boolean>;
+
+export const isFeatureFlag = createEnumGuard(FeatureFlags);
+
+const bootstrapFlagState = () => {
+  const q = new URLSearchParams(location.search);
+  const initialFeatures = (q.get('features') || '')
+    .split(',')
+    .map((s) => s.trim())
+    .filter(isFeatureFlag);
+
+  return new Map(initialFeatures.map((f) => [f, true]));
+};
 
 export const useFeatureFlags = () => {
+  const [initialOverrides] = useState(() => bootstrapFlagState());
   const features = useQuery(api.features.loadFeatureFlags);
   const isLoading = features === undefined;
   const featuresMap = useMemo<FeaturesMap>(
     () =>
-      new Map(
-        (features || []).map((feat) => [feat.key as FeatureFlags, feat.value]),
-      ),
-    [features],
+      (features || []).reduce<FeaturesMap>((res, feat) => {
+        if (isFeatureFlag(feat.key)) {
+          res.set(feat.key, initialOverrides.get(feat.key) || feat.value);
+        }
+
+        return res;
+      }, new Map()),
+
+    [features, initialOverrides],
   );
 
   return {
