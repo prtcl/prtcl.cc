3f1e40479f006ebae0041bab663dac6227e28c38
Cory O'Brien <cory@prtcl.cc>
2025-01-03 12:23:54 -0500

    render embed viewer in fixed container with scale transition

--- src/feat/Projects/components/EmbedViewer.tsx
+++ src/feat/Projects/components/EmbedViewer.tsx
@@ -1,5 +1,6 @@
+import { animated, useTransition } from '@react-spring/web';
 import { useQuery } from 'convex/react';
-import { Flex, type FlexProps } from 'styled-system/jsx';
+import { Flex, styled, type FlexProps } from 'styled-system/jsx';
 import { api } from '~/convex/api';
 import { MediaEmbed } from '~/ui/MediaEmbed';
 import type { ProjectId } from '../types';
@@ -7,22 +8,33 @@ import type { ProjectId } from '../types';
 const Container = (props: FlexProps) => {
   const { children, ...flexProps } = props;
   return (
-    <Flex
-      borderRadius={12}
-      flex={1}
-      height="fit-content"
-      minHeight="fit-content"
-      opacity={0}
-      overflow="hidden"
-      shadow="2xl"
-      transition="opacity 168ms cubic-bezier(.79,.31,.59,.83)"
-      width="100%"
-      {...flexProps}
-    >
-      {children}
+    <Flex px={0.5}>
+      <Flex
+        borderRadius={12}
+        flex={1}
+        height="fit-content"
+        minHeight="fit-content"
+        overflow="hidden"
+        shadow="2xl"
+        transition="opacity 168ms cubic-bezier(.79,.31,.59,.83)"
+        width="100%"
+        {...flexProps}
+      >
+        {children}
+      </Flex>
     </Flex>
   );
 };
+
+const ViewerPosition = styled(animated.div, {
+  base: {
+    height: 'fit-content',
+    left: 0,
+    position: 'fixed',
+    top: 0,
+    width: '100%',
+  },
+});
 
 export const EmbedViewer = (props: {
   projectId: ProjectId;
@@ -30,16 +42,27 @@ export const EmbedViewer = (props: {
 }) => {
   const { projectId, isOpen } = props;
   const embedCode = useQuery(api.projects.loadProjectEmbed, { projectId });
+  const transitions = useTransition(isOpen && embedCode, {
+    from: { opacity: 0, scale: 0.88 },
+    enter: { opacity: 1, scale: 1 },
+    leave: { opacity: 0, scale: 0.88 },
+  });
 
-  return (
-    <Container opacity={embedCode === undefined ? 0 : 1}>
-      {embedCode && isOpen && (
-        <MediaEmbed
-          service={embedCode.service}
-          src={embedCode.src}
-          title={embedCode.title}
-        />
-      )}
-    </Container>
-  );
+  return transitions((styles, shouldRender) => {
+    if (shouldRender) {
+      return (
+        <ViewerPosition style={styles}>
+          <Container>
+            <MediaEmbed
+              service={embedCode.service}
+              src={embedCode.src}
+              title={embedCode.title}
+            />
+          </Container>
+        </ViewerPosition>
+      );
+    }
+
+    return null;
+  });
 };
