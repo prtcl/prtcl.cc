f1fc64421e281e09662303be90a2a9fc49168feb
Cory O'Brien <cory@prtcl.cc>
2025-02-13 11:15:59 -0500

    extract invariants and refactor

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -15,6 +15,8 @@ import type {
 } from "convex/server";
 import type * as features from "../features.js";
 import type * as internal_ from "../internal.js";
+import type * as lib_invariants from "../lib/invariants.js";
+import type * as lib_types from "../lib/types.js";
 import type * as migrations from "../migrations.js";
 import type * as projects from "../projects.js";
 
@@ -29,6 +31,8 @@ import type * as projects from "../projects.js";
 declare const fullApi: ApiFromModules<{
   features: typeof features;
   internal: typeof internal_;
+  "lib/invariants": typeof lib_invariants;
+  "lib/types": typeof lib_types;
   migrations: typeof migrations;
   projects: typeof projects;
 }>;
