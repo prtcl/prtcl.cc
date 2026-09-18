9f14b4ad493b618bcbf37f588d38945b1d5ea00d
Cory O'Brien <cory@prtcl.cc>
2026-09-19 00:41:53 +0100

    extend link component with router variant

--- src/ui/Link.tsx
+++ src/ui/Link.tsx
@@ -1,13 +1,43 @@
+import { Link as TsLinkBase } from '@tanstack/react-router';
 import { styled } from 'styled-system/jsx';
+import type { RecipeDefinition } from 'styled-system/types';
+
+const linkStyles: RecipeDefinition['base'] = {
+  color: 'text',
+  cursor: 'pointer',
+  textDecoration: 'none',
+};
+
+const variantStyles: RecipeDefinition['variants'] = {
+  visual: {
+    underline: {
+      borderBottom: '1px solid currentColor',
+      width: 'fit-content',
+      _hover: {
+        borderBottomColor: 'transparent',
+      },
+    },
+    basic: {
+      width: '100%',
+      _active: {
+        opacity: 0.75,
+      },
+    },
+  },
+};
 
 export const Link = styled('a', {
-  base: {
-    borderBottom: '1px solid currentColor',
-    color: 'text',
-    textDecoration: 'none',
-    width: 'fit-content',
-    _hover: {
-      borderBottomColor: 'transparent',
-    },
+  base: { ...linkStyles },
+  variants: { ...variantStyles },
+  defaultVariants: {
+    visual: 'underline',
+  },
+});
+
+export const TsLink = styled(TsLinkBase, {
+  base: { ...linkStyles },
+  variants: { ...variantStyles },
+  defaultVariants: {
+    visual: 'underline',
   },
 });
